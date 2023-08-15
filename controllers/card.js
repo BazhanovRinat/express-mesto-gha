de78d00e676ac83d8ccce33367a85ab174c4a463
Rinat Bazhanov <Bazhanov-rinat@yandex.ru>
2023-08-15 13:19:01 +0300

    Revert "develop-mesto-backend(2)"
    This reverts commit d8e03d7991f8bf114525bc87a5008cd2550282c0.

--- controllers/card.js
+++ controllers/card.js
@@ -37,9 +37,9 @@ const deleteCard = (req, res) => {
             return res.status(200).send({ message: "Карточка удалена" })
         })
         .catch((err) => {
-            // if (err.name === 'CastError') {
-            //     return res.status(404).send({ message: "Неправильный Id карточки" })
-            // }
+            if (err.name === 'CastError') {
+                return res.status(404).send({ message: "Неправильный Id карточки" })
+            }
             console.log(err)
             return res.status(404).send({ message: "server error" })
         })
@@ -71,9 +71,9 @@ const likeCard = (req, res) => {
             return res.status(200).send({ message: "Лайк поставлен" })
         })
         .catch((err) => {
-            // if (err.name === 'CastError') {
-            //     return res.status(404).send({ message: "Неправильный Id карточки" })
-            // }
+            if (err.name === 'CastError') {
+                return res.status(404).send({ message: "Неправильный Id карточки" })
+            }
             console.log(err)
             return res.status(404).send({ message: "server error" })
         })
@@ -98,9 +98,9 @@ const dislakeCards = (req, res) => {
             return res.status(200).send({ message: "Лайк убран" })
         })
         .catch((err) => {
-            // if (err.name === 'CastError') {
-            //     return res.status(404).send({ message: "Неправильный Id карточки" })
-            // }
+            if (err.name === 'CastError') {
+                return res.status(404).send({ message: "Неправильный Id карточки" })
+            }
             console.log(err)
             return res.status(404).send({ message: "server error" })
         })
